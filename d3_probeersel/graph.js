6721f4777c80d0c2788146aa8da28e50f8653062
JensNys <jens.nys@student.kuleuven.be>
2025-08-06 09:27:25 +0200

    error handling and svg from data fixed

--- d3_probeersel/graph.js
+++ d3_probeersel/graph.js
@@ -6,7 +6,7 @@ async function draw(){
   
   //const data = await d3.json("example.json");
   //
-  data = {svg:{id:0,width:800,height:300},
+  data = {svg:{id:0,width:800,height:400},
     circles:[{id:1, r: 40,cx:50,cy:50,color:"red"},
       {id:2, r: 40,cx:150,cy:50,color:"blue"},
       {id:3, r: 40,cx:150,cy:150,color:"yellow"},
@@ -15,11 +15,12 @@ async function draw(){
       {id:5,from:2,to:3,color:"black"}
   ]}
   
-  let svg = d3.select('body')//.append("p").text("boe");
-  .append("svg")
-  .attr('width', 600)
-  .attr('height', 300)
-  .attr('id',0),
+  let svg = d3.selectAll('svg')//.append("p").text("boe");
+  .data([data.svg])
+  .join("svg")
+  .attr('width', d=>d.width ?? 600)
+  .attr('height',d=>d.height ?? 300)
+  .attr('id',d=>d.id ?? 0),
   margin = { top: 50, right: 50, bottom: 50, left: 50 };
 
   
@@ -31,13 +32,14 @@ async function draw(){
   const id_to_circle = d3.rollup(data.circles,e=>e[0],d=>d.id)
   
   svg.selectAll("line")
-      .data(data.links)
-      .join("line")
-      .attr("x1", d => (id_to_circle.get(d.from).cx))
-      .attr("y1", d => id_to_circle.get(d.from).cy)
-      .attr("x2", d => id_to_circle.get(d.to).cx)
-      .attr("y2", d => id_to_circle.get(d.to).cy)
-      .attr("stroke",d=>d.color);
+  .data(data.links)
+  .join("line")
+  .attr("x1", d => id_to_circle.get(d.from)?.cx ?? 0)
+  .attr("y1", d => id_to_circle.get(d.from)?.cy ?? 0)
+  .attr("x2", d => id_to_circle.get(d.to)?.cx ?? 0)
+  .attr("y2", d => id_to_circle.get(d.to)?.cy ?? 0)
+  .attr("stroke", d => d.color ?? "black");
+
   
   svg.selectAll("circle")
       .data(data.circles)
