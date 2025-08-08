525214d467fe67c8a6027a63c76d87ebffa427c5
JensNys <jens.nys@student.kuleuven.be>
2025-08-08 08:12:38 +0200

    some files missing

---
+++ python_part_for_real/out/script.js
@@ -0,0 +1,66 @@
+
+async function draw(){
+
+
+
+  
+    const data = await d3.json("visualisation.json");
+    
+    // data = {svg:{id:0,width:800,height:400},
+    //   circles:[{id:1, r: 40,cx:50,cy:50,color:"red"},
+    //     {id:2, r: 40,cx:150,cy:50,color:"blue"},
+    //     {id:3, r: 40,cx:150,cy:150,color:"yellow"},
+    // ],links:[
+    //     {id:4,from:1,to:2,color:"black"},
+    //     {id:5,from:2,to:3,color:"black"}
+    // ]}
+    
+    let svg = d3.selectAll('svg')//.append("p").text("boe");
+    .data([data.svg])
+    .join("svg")
+    .attr('width', d=>d?.width ?? 600)
+    .attr('height',d=>d?.height ?? 300)
+    .attr('id',d=>d?.id ?? 0),
+    margin = { top: 50, right: 50, bottom: 50, left: 50 };
+  
+    
+    
+    
+    
+    
+    //de gewone lookup geeft een lijst terug, maar gezien id uniek is krijgen we singleton lijsten. Daarom nog [0]
+    const id_to_circle = d3.rollup(data.circles,e=>e[0],d=>d.id)
+    
+    svg.selectAll("line")
+    .data(data.links)
+    .join("line")
+    .attr("x1", d => id_to_circle.get(d.from)?.cx ?? 0)
+    .attr("y1", d => id_to_circle.get(d.from)?.cy ?? 0)
+    .attr("x2", d => id_to_circle.get(d.to)?.cx ?? 0)
+    .attr("y2", d => id_to_circle.get(d.to)?.cy ?? 0)
+    .attr("stroke", d => d.color ?? "black");
+  
+    
+    svg.selectAll("circle")
+        .data(data.circles)
+        .join("circle")
+        .attr("cx", d => d.cx)
+        .attr("cy", d => d.cy)
+        .attr("r", d => d.r)
+        .attr("fill",d=>d.color);
+
+    svg.selectAll("rect")
+      .data(data.rects)
+      .join("rect")
+      .attr('x', d=>d.x)
+      .attr('y', d=>d.y)
+      .attr('width', d=>d.width)
+      .attr('height', d=>d.height)
+      .attr('fill', d=>d.color)
+
+    
+  }
+  
+  draw();
+  
+  
